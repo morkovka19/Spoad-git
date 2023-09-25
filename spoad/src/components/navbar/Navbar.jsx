523ea0e925801e8c3b21f6e6800faaa9eb4e56d9
Polina Shapovalova <shapovalova.p.d@gmail.com>
2023-09-25 18:56:11 +0300

    add navbar

--- spoad/src/components/navbar/Navbar.jsx
+++ spoad/src/components/navbar/Navbar.jsx
@@ -7,6 +7,9 @@ export default function Navbar({onLoginOut}){
             <ul className='navbar__list'>
                 <li className='navbar__item'><Link to='/github' className='navbar__link' >GitHub</Link></li>
                 <li className='navbar__item'><Link  to='/aws' className='navbar__link' >AWS</Link></li>
+                <li className='navbar__item'><Link  to='/urlink' className='navbar__link' >Urlink</Link></li>
+                <li className='navbar__item'><Link  to='/couldocker' className='navbar__link' >Couldocker</Link></li>
+                <li className='navbar__item'><Link  to='/crackcroack' className='navbar__link' >Crackcroack</Link></li>
                 <li className='navbar__item'><Link  to='/:login' className='navbar__link' >Settings</Link></li>
                 <li className='navbar__item'><button type='button' className='navbar__button' onClick={onLoginOut}>Sign out</button></li>
             </ul>
